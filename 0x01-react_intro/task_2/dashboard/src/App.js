d7b3b0e4f15a80706596e73210410ee29644d3e1
bowhle <bowhlem@gmailcom>
2025-05-15 01:02:34 +0200

    Modify the Notifications

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -8,38 +8,32 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className='header-content'>
-        <img src={ALX_logo} className="ALX-logo" alt="ALX Logo" />
-        <h1>School dashboard</h1>
+          <img src={ALX_logo} className="ALX-logo" alt="ALX Logo" />
+          <h1>School dashboard</h1>
         </div>
         <hr className='header-separator' />
       </header>
 
-  <div className='App-body'>
-    <p>Login to access the full dashboard</p>
-    <div className='login-form'>
-      <div className='form-group'>
-        <label htmlFor='email'>Email:</label>
-        <input type='email'
-        id='email'
-        name='email' 
-        placeholder='Enter your email' />
-    </div>
-    <div className='form-group'>
-        <label htmlFor='password'>Password:</label>
-        <input type='password'
-        id='password'
-        name='password'
-        placeholder='Enter your password' />
-    </div>
-    <div className='submit-btn'>
-        <button type='submit'>Login</button>
-    </div>
-    </div>
-  
-  <footer className='App-footer'>
-    <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-  </footer>
-  </div>
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+        <div className='login-form'>
+          <div className='form-group'>
+            <label htmlFor='email'>Email:</label>
+            <input type='email' id='email' name='email' placeholder='Enter your email' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='password'>Password:</label>
+            <input type='password' id='password' name='password' placeholder='Enter your password' />
+          </div>
+          <div className='submit-btn'>
+            <button type='submit'>Login</button>
+          </div>
+        </div>
+      </div>
+
+      <footer className='App-footer'>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
     </div>
   );
 }
