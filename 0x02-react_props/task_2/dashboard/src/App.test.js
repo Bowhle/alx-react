8e5f2239ab53f242607bf550e1aa18f89e43f448
bowhle <bowhlem@gmailcom>
2025-05-22 04:05:11 +0200

    App test

--- 0x02-react_props/task_2/dashboard/src/App.test.js
+++ 0x02-react_props/task_2/dashboard/src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from "@testing-library/react";
 import App from './App';
 
-const wrapper = shallow(<App />);
+test('App contains the Notifications component', () => {
+  render(<App />);
+  expect(screen.getByText(/Here is the list of notifications/i)).toBeInTheDocument();
+});
 
-it('renders without crashing', () => {
-  shallow(<App />);
+test('App contains the Header component', () => {
+  render(<App />);
+  expect(screen.getByText(/School dashboard/i)).toBeInTheDocument();
+});
+
+test('App contains the Login component', () => {
+  render(<App />);
+  expect(screen.getByText(/Login to access the full dashboard/i)).toBeInTheDocument();
+});
+
+test('App contains the Footer component', () => {
+  render(<App />);
+  expect(screen.getByText(/Copyright/i)).toBeInTheDocument();
 });
